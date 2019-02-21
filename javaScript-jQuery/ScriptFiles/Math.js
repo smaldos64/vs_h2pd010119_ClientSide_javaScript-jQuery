e43cac079809e9c143fc33bec1e6d11b294e0d1f
Lars Thise Pedersen <ltpe@techcollege.dk>
2019-02-22 00:13:11 +0100

    1) Implementeret javaScript lommeregner. 2) Indført seperat javaScript fil. 3) Fjernet alt udkommenteret kode.

---
+++ javaScript-jQuery/ScriptFiles/Math.js
@@ -0,0 +1,49 @@
+﻿MathArray = [];
+
+Counter = 0;
+MathArray.push({
+    Operator: '+', OperatorValue: Counter++, MathOperation: function (a, b) { return a + b }
+});
+
+MathArray.push({
+    Operator: '-', OperatorValue: Counter++, MathOperation: function (a, b) { return a - b }
+});
+
+MathArray.push({
+    Operator: '*', OperatorValue: Counter++, MathOperation: function (a, b) { return a * b }
+});
+
+MathArray.push({
+    Operator: '/', OperatorValue: Counter++, MathOperation: function (a, b) { return a / b }
+});
+
+MathArray.push({
+    Operator: '%', OperatorValue: Counter++, MathOperation: function (a, b) { return a / b * 100 }
+});
+
+function ClearTextBoxes(TextBoxArray) {
+    Counter = 0;
+    while (Counter < TextBoxArray.length) {
+        TextBoxArray[Counter].value = "";
+        Counter++;
+    }
+}
+
+function CalculateMathResult(MathOperation, Value1String, Value2String) {
+    Counter = 0;
+    MathOperationFound = false;
+
+    while ((Counter < MathArray.length) && (false == MathOperationFound)) {
+        if (MathArray[Counter].OperatorValue == MathOperation) {
+            MathOperationFound = true;
+            return (MathArray[Counter].MathOperation(parseFloat(Value1String), parseFloat(Value2String)).toString());
+        }
+        else {
+            Counter++;
+        }
+    }
+
+    if (false == MathOperationFound) {
+        alert("Der er noget galt i dit program ---> Spaghetti programmør !!!");
+    }
+}
